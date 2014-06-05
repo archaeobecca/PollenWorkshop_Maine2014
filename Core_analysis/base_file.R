99b95c9d670969b0b77ca8e7711efaf3383aaec7
archaeobecca <Becca@Rebeccas-MacBook-Pro.local>
2014-06-05 16:24:53 -0400

    Tried to write up code to plot Ulmus pollen against some climate variables, had some issues with the data from downloaded package 'analogue'.

--- Core_analysis/base_file.R
+++ Core_analysis/base_file.R
@@ -1,2 +1,14 @@
 #  Understanding pollen and climate relationships.
-#  Simon Goring
+#  Rebecca Friedel
+
+pollen <- read.csv('Core_analysis/data/input/mpd_pollen.csv')
+climate <- read.csv('Core_analysis/data/input/mpd_climate.csv')
+
+# There are a bunch of NA values at the end of the pollen data for some reason.
+pollen.sub <- pollen[!is.na(pollen$ID2),] 
+
+plot(y=pollen.sub$ULMUS/pollen.sub$POLLSUM, x=climate$pjul)
+
+install.packages('analogue')
+
+library(analogue)
